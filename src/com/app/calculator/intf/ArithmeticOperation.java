ffeb19221af936839180bb939bf2ac62fcc515ab
Xanaxiel <cyril.pangilinan@gmail.com>
2017-02-28 06:20:21 +0800

    Updated javadoc

--- src/com/app/calculator/intf/ArithmeticOperation.java
+++ src/com/app/calculator/intf/ArithmeticOperation.java
@@ -7,9 +7,19 @@ package com.app.calculator.intf;
  * 
  * @author Cy
  * @param <T> - a number such that type {@code T} is a primitive {@code byte, double, float, int, long,} and {@code short}
+ * 
  * @see Number
  * 
  */
 public interface ArithmeticOperation<T extends Number> {
+	/**
+	 * <pre>
+	 * Provide an implementation in the concrete class such that the operation is executed on the number type.
+	 * <pre>
+	 * 
+	 * @param firstInput 
+	 * @param secondInput
+	 * @return the result of the operation
+	 */
 	T computeResult(T firstInput, T secondInput);
 }
